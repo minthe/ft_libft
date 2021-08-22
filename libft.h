b2e38b3896594285ee28b845eab53250676ab793
Valentin Fuhlenbrok <vfuhlenb@c2s3d6.42wolfsburg.de>
2021-08-22 17:38:29 +0200

    progress

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vfuhlenb <vfuhlenb@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/10 11:00:50 by vfuhlenb          #+#    #+#             */
-/*   Updated: 2021/08/22 17:13:39 by vfuhlenb         ###   ########.fr       */
+/*   Updated: 2021/08/22 17:31:26 by vfuhlenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);

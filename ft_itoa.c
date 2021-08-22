0a122f52cec9107525caf1f1b3ab78beeaf0477d
Valentin Fuhlenbrok <vfuhlenb@c2s3d6.42wolfsburg.de>
2021-08-22 20:17:54 +0200

    progress

--- ft_itoa.c
+++ ft_itoa.c
@@ -5,48 +5,56 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: vfuhlenb <vfuhlenb@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/22 18:15:35 by vfuhlenb          #+#    #+#             */
-/*   Updated: 2021/08/22 18:44:23 by vfuhlenb         ###   ########.fr       */
+/*   Created: 2021/08/22 20:11:30 by vfuhlenb          #+#    #+#             */
+/*   Updated: 2021/08/22 20:16:39 by vfuhlenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_isneg(int n)
+static size_t	ft_countd(int n)
 {
+	size_t	x;
+
+	x = 0;
 	if (n < 0)
-		return (1);
-	else
-		return (-1);
-}
-
-static size_t	ft_ndigits(int n, int neg)
-{
-	size_t	nd;
-
-	if (neg == 1)
-		nd = 2;
-	else
-		nd = 1;
-	while (n / 10)
+	{
+		n *= -1;
+		++x;
+	}
+	while (n >= 10)
 	{
 		n /= 10;
-		nd++;
+		++x;
 	}
-	return (nd);
+	return (++x);
 }
 
 char	*ft_itoa(int n)
 {
-	char	*str;
-	int		neg;
+	char	*d;
+	char	*r;
+	size_t	dc;
 
-	neg = ft_isneg(n);
-	n = n * neg;
-	str = (char *)malloc(sizeof(char) * (ft_ndigits(n, neg) + 1));
-	if (!str)
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	dc = ft_countd(n);
+	d = ft_calloc(sizeof(char), (dc + 1));
+	if (!d)
 		return (NULL);
-	if (neg == 1)
-		*str++ = '-';
-	while()
+	r = d;
+	if (n < 0)
+	{
+		*d = '-';
+		n *= -1;
+	}
+	d += dc;
+	while (n >= 10)
+	{
+		d--;
+		*d = ((n % 10) + '0');
+		n /= 10;
+	}
+	*(--d) = (n % 10) + '0';
+	return (r);
 }
